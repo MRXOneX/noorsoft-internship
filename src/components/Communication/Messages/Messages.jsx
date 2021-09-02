d51e7acf77aafffcd75af1a70e68e85b0ffbaa58
MRXOneX <mikhail.poleshchenkov@mail.ru>
2021-09-02 13:04:15 +0300

    [TASK-22]: I added the necessary components for the completed dialogs page and implemented all the functionality.

--- src/components/Communication/Messages/Messages.jsx
+++ src/components/Communication/Messages/Messages.jsx
@@ -2,16 +2,26 @@ import MessagesHeader from "./MessagesComponents/MessagesHeader";
 import MessagesBlock from "./MessagesComponents/MessagesBlock";
 import MessagesWrite from "./MessagesComponents/MessagesWrite";
 
-import styles from "./Messages.module.css";
 import { useDialog } from "../../../redux/selectors";
 
+import styles from "./Messages.module.css";
+
 const Messages = () => {
+  const location = window.location.pathname;
   const dialog = useDialog();
   return (
     <div className={styles.messages}>
       <MessagesHeader {...dialog} />
       <MessagesBlock dialog={dialog} />
-      <MessagesWrite />
+      {location === `/complected/messages/${dialog.id}` ? (
+        <div className={styles.complectedDialogText}>
+          <p>
+            Диалог с <span>{dialog.name}</span> завершен
+          </p>
+        </div>
+      ) : (
+        <MessagesWrite />
+      )}
     </div>
   );
 };
