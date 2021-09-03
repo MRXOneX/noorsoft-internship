e1e49ea0d9c2cd49482eda7ada85fd5e6da65105
MRXOneX <mikhail.poleshchenkov@mail.ru>
2021-09-03 14:27:14 +0300

    [FIXED-7]: I moved the styles from Avatar.jsx in Messages.module.css

--- src/components/Communication/Messages/MessagesComponents/Message/MessageComponents/Avatar.jsx
+++ src/components/Communication/Messages/MessagesComponents/Message/MessageComponents/Avatar.jsx
@@ -1,18 +1,10 @@
 import Title from "../../../../../utils/Title";
 
+import styles from "../../../Messages.module.css";
+
 const Avatar = () => {
   return (
-    <div
-      style={{
-        borderRadius: "5px",
-        background: "rgba(146, 143, 253)",
-        color: "white",
-        width: "40px",
-        height: "40px",
-        fontWeight: "550",
-        textAlign: "center",
-      }}
-    >
+    <div className={styles.avatar}>
       <Title size={30} title="O" showHr={false} />
     </div>
   );
